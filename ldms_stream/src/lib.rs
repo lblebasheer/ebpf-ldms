a2aeeac20aee5722465a732fec95fce87c7141f8
Ershaad Basheer <ebasheer@lbl.gov>
2025-06-23 15:36:22 -0700

    rustfmt

--- ldms_stream/src/lib.rs
+++ ldms_stream/src/lib.rs
@@ -1,5 +1,5 @@
-use std::ffi::{CString};
-use anyhow::{anyhow, Result};
+use anyhow::{Result, anyhow};
+use std::ffi::CString;
 use std::ptr;
 
 mod raw;
@@ -22,7 +22,10 @@ impl SockStream {
         let c_auth = match auth {
             "none" => Ok(CString::new(auth)?),
             "munge" => Ok(CString::new(auth)?),
-            _ => Err(anyhow!("Unsupported or unimplemented authentication: {}", auth)),
+            _ => Err(anyhow!(
+                "Unsupported or unimplemented authentication: {}",
+                auth
+            )),
         }?;
         let c_host = CString::new(host)?;
         let c_port = CString::new(port)?;
@@ -34,7 +37,9 @@ impl SockStream {
             raw::ldms_xprt_new_with_auth(c_xprt.as_ptr(), null_log, c_auth.as_ptr(), null_value)
         };
         if xprt_handle.is_null() {
-            return Err(anyhow!("ldms_xprt_new_with_auth: Failed to create new transport"));
+            return Err(anyhow!(
+                "ldms_xprt_new_with_auth: Failed to create new transport"
+            ));
         }
         Ok(SockStream {
             c_host,
@@ -49,10 +54,19 @@ impl SockStream {
         let null_arg: *const raw::cb_arg = ptr::null();
 
         let rc = unsafe {
-            raw::ldms_xprt_connect_by_name(self.xprt_handle, self.c_host.as_ptr(), self.c_port.as_ptr(), null_event, null_arg)
+            raw::ldms_xprt_connect_by_name(
+                self.xprt_handle,
+                self.c_host.as_ptr(),
+                self.c_port.as_ptr(),
+                null_event,
+                null_arg,
+            )
         };
         if rc != 0 {
-            return Err(anyhow!("ldms_xprt_connect_by_name: Failed to connect transport: {}", rc));
+            return Err(anyhow!(
+                "ldms_xprt_connect_by_name: Failed to connect transport: {}",
+                rc
+            ));
         }
         Ok(())
     }
@@ -66,7 +80,8 @@ impl SockStream {
                 self.c_stream.as_ptr(),
                 raw::ldmsd_stream_type_t::LDMSD_STREAM_JSON,
                 c_message.as_ptr(),
-                (c_message.count_bytes() + 1) as u64)
+                (c_message.count_bytes() + 1) as u64,
+            )
         };
         if rc != 0 {
             return Err(anyhow!("ldmsd_steam_publish: failed: {}", rc));
@@ -74,4 +89,3 @@ impl SockStream {
         Ok(())
     }
 }
-
--- ldms_stream/src/raw.rs
+++ ldms_stream/src/raw.rs
@@ -1,33 +1,51 @@
 use std::ffi::{c_char, c_int, c_ulonglong};
 
-#[repr(C)] pub struct ldms_log_fn_t { _private: [u8; 0] }
-#[repr(C)] pub struct attr_value_list { _private: [u8; 0] }
-#[repr(C)] pub struct ldms_t { _private: [u8; 0] }
-#[repr(C)] pub struct ldms_event_cb_t { _private: [u8; 0] }
-#[repr(C)] pub struct cb_arg { _private: [u8; 0] }
+#[repr(C)]
+pub struct ldms_log_fn_t {
+    _private: [u8; 0],
+}
+#[repr(C)]
+pub struct attr_value_list {
+    _private: [u8; 0],
+}
+#[repr(C)]
+pub struct ldms_t {
+    _private: [u8; 0],
+}
+#[repr(C)]
+pub struct ldms_event_cb_t {
+    _private: [u8; 0],
+}
+#[repr(C)]
+pub struct cb_arg {
+    _private: [u8; 0],
+}
 
-#[link(name = "ldms",)]
+#[link(name = "ldms")]
 #[link(name = "ldmsd_stream")]
 unsafe extern "C" {
     pub fn ldms_xprt_new_with_auth(
-            xprt_name: *const c_char,
-            log_fn: *const ldms_log_fn_t,
-            auth_name: *const c_char,
-            auth_av_list: *const attr_value_list) -> *const ldms_t;
+        xprt_name: *const c_char,
+        log_fn: *const ldms_log_fn_t,
+        auth_name: *const c_char,
+        auth_av_list: *const attr_value_list,
+    ) -> *const ldms_t;
 
     pub fn ldms_xprt_connect_by_name(
-            x: *const ldms_t,
-            host: *const c_char,
-            port: *const c_char,
-            cb: *const ldms_event_cb_t,
-            cb_arg: *const cb_arg) -> c_int;
+        x: *const ldms_t,
+        host: *const c_char,
+        port: *const c_char,
+        cb: *const ldms_event_cb_t,
+        cb_arg: *const cb_arg,
+    ) -> c_int;
 
     pub fn ldmsd_stream_publish(
-            x: *const ldms_t,
-            stream_name: *const c_char,
-            stream_type: ldmsd_stream_type_t,
-            data: *const c_char,
-            data_len: c_ulonglong) -> c_int;
+        x: *const ldms_t,
+        stream_name: *const c_char,
+        stream_type: ldmsd_stream_type_t,
+        data: *const c_char,
+        data_len: c_ulonglong,
+    ) -> c_int;
 }
 
 #[repr(C)]
