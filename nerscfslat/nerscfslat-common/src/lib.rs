f683e946f74b1c95a18f171dce21ead79d61bcb7
Ershaad Ahamed Basheer <ebasheer@lbl.gov>
2025-12-26 01:57:20 +0000

    Move write to ring buffer into function

--- nerscfslat/nerscfslat-common/src/lib.rs
+++ nerscfslat/nerscfslat-common/src/lib.rs
@@ -1,1 +1,13 @@
 #![no_std]
+use aya_ebpf::{
+    bindings::bpf_spin_lock,
+};
+
+#[repr(C)]
+pub struct FsWriteStats {
+    pub lock: bpf_spin_lock,
+    pub min: u64,
+    pub max: u64,
+    pub total: u64,
+    pub count: u64,
+}
--- nerscfslat/nerscfslat-ebpf/src/main.rs
+++ nerscfslat/nerscfslat-ebpf/src/main.rs
@@ -2,12 +2,14 @@
 #![no_main]
 
 use aya_ebpf::{
+    cty::c_char,
     helpers::bpf_ktime_get_ns,
     macros::{fentry, fexit, map},
     maps::{Array, HashMap, RingBuf},
     programs::{FEntryContext, FExitContext},
 };
 use minicbor::Encoder;
+use nerscfslat_common::FsWriteStats;
 
 #[allow(nonstandard_style)]
 #[allow(unnecessary_transmutes)]
@@ -24,7 +26,17 @@ static COUNTER: Array<u64> = Array::with_max_entries(1, 0);
 static PTRLIST: HashMap<usize, u64> = HashMap::with_max_entries(1024, 0);
 
 #[map]
+static WRITESTATS: HashMap<[c_char; 32], FsWriteStats> = HashMap::with_max_entries(16, 0);
+
+#[map]
 static LDMS_SHARED_STREAM: RingBuf = RingBuf::pinned(8192, 0);
+
+struct EventFields<'a> {
+    id: &'a str,
+    version: &'a str,
+    monotonic: u64,
+    seq: u64,
+}
 
 #[fentry(function = "filp_close")]
 pub fn filp_close_entry(ctx: FEntryContext) -> u32 {
@@ -55,60 +67,82 @@ fn try_fslat_exit(ctx: FExitContext) -> Result<u32, u32> {
     let Some(countptr) = COUNTER.get_ptr_mut(0) else {
         return Err(1);
     };
+    let eventf = EventFields {
+        id: "fslat",
+        version: "v1",
+        monotonic: now,
+        seq: unsafe { *countptr },
+    };
 
     match unsafe { PTRLIST.get(filp as usize) } {
         Some(nsecs) => {
             let delta = now - nsecs;
             if delta > 10 {
-                let Some(mut dataent) = LDMS_SHARED_STREAM.reserve::<[u8; BUFSIZE]>(0) else {
+                let Ok(_) = ringbuf_put(&eventf, "filp_close", delta, "ns") else {
                     return Err(1);
                 };
-                let dataent_bytes: *mut [u8] = dataent.as_mut_ptr();
-                let mut encoder = unsafe { Encoder::new(&mut *dataent_bytes) };
-                unsafe {
-                    encoder
-                          .begin_map()
-                          .unwrap_unchecked()
-                          .str("id")
-                          .unwrap_unchecked()
-                          .str("fslat")
-                          .unwrap_unchecked()
-                          .str("version")
-                          .unwrap_unchecked()
-                          .str("v1")
-                          .unwrap_unchecked()
-                          .str("timestamp_monotonic")
-                          .unwrap_unchecked()
-                          .u64(now)
-                          .unwrap_unchecked()
-                          .str("metrics")
-                          .unwrap_unchecked()
-                              .begin_array()
-                              .unwrap_unchecked()
-                                  .begin_map()
-                                  .unwrap_unchecked()
-                                      .str("sequence")
-                                      .unwrap_unchecked()
-                                      .u64(*countptr)
-                                      .unwrap_unchecked()
-                                      .str("latency")
-                                      .unwrap_unchecked()
-                                      .u64(delta)
-                                      .unwrap_unchecked()
-                                  .end()
-                                  .unwrap_unchecked()
-                              .end()
-                              .unwrap_unchecked()
-                          .end()
-                          .unwrap_unchecked();
-                }
-                dataent.submit(0);
                 let _ = PTRLIST.remove(&(filp as usize));
             }
         unsafe { *countptr += 1; }
         }
         None => {},
     };
+    Ok(0)
+}
+
+fn ringbuf_put(eventf: &EventFields, op_name: &str, latency: u64, unit: &str) -> Result<u32, u32> {
+    let Some(mut dataent) = LDMS_SHARED_STREAM.reserve::<[u8; BUFSIZE]>(0) else {
+        return Err(1);
+    };
+    let EventFields{id, version, monotonic, seq} = *eventf;
+    let dataent_bytes: *mut [u8] = dataent.as_mut_ptr();
+    let mut encoder = unsafe { Encoder::new(&mut *dataent_bytes) };
+    unsafe {
+        encoder
+              .begin_map()
+              .unwrap_unchecked()
+              .str("id")
+              .unwrap_unchecked()
+              .str(id)
+              .unwrap_unchecked()
+              .str("version")
+              .unwrap_unchecked()
+              .str(version)
+              .unwrap_unchecked()
+              .str("timestamp_monotonic")
+              .unwrap_unchecked()
+              .u64(monotonic)
+              .unwrap_unchecked()
+              .str("metrics")
+              .unwrap_unchecked()
+                  .begin_array()
+                  .unwrap_unchecked()
+                      .begin_map()
+                      .unwrap_unchecked()
+                          .str("sequence")
+                          .unwrap_unchecked()
+                          .u64(seq)
+                          .unwrap_unchecked()
+                          .str("latency")
+                          .unwrap_unchecked()
+                          .u64(latency)
+                          .unwrap_unchecked()
+                          .str("unit")
+                          .unwrap_unchecked()
+                          .str(unit)
+                          .unwrap_unchecked()
+                          .str("operation")
+                          .unwrap_unchecked()
+                          .str(op_name)
+                          .unwrap_unchecked()
+                      .end()
+                      .unwrap_unchecked()
+                  .end()
+                  .unwrap_unchecked()
+              .end()
+              .unwrap_unchecked();
+    }
+    dataent.submit(0);
     Ok(0)
 }
 
