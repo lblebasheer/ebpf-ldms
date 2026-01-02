0832ea4e85d035eafa30fae4b98a5fd581ee4464
Ershaad Ahamed Basheer <ebasheer@lbl.gov>
2026-01-02 06:34:42 +0000

    Store latency statistics in WRITESTATS map

--- nerscfslat/nerscfslat-common/src/lib.rs
+++ nerscfslat/nerscfslat-common/src/lib.rs
@@ -1,16 +1,23 @@
 #![no_std]
-use aya_ebpf::{bindings::bpf_spin_lock, cty::c_uchar};
+use aya_ebpf::cty::c_uchar;
 
 pub const PATHFRAGLEN: usize = 32 + 1;
 pub const NUM_PATH_PREFIX: u32 = 8;
 
 #[repr(C)]
+#[derive(Copy, Clone)]
 pub struct FsWriteStats {
-    pub lock: bpf_spin_lock,
     pub pathfrag: [c_uchar; PATHFRAGLEN],
     pub fraglen: usize,
     pub min: u64,
     pub max: u64,
     pub total: u64,
     pub count: u64,
+}
+
+#[repr(C)]
+pub struct EntryRec {
+    pub timestamp: u64,
+    pub pathfrag: [c_uchar; PATHFRAGLEN],
+    pub fraglen: usize,
 }
--- nerscfslat/nerscfslat-ebpf/src/main.rs
+++ nerscfslat/nerscfslat-ebpf/src/main.rs
@@ -1,17 +1,19 @@
 #![no_std]
 #![no_main]
 
+use core::borrow::BorrowMut;
+
 use aya_ebpf::{
     bindings::path,
-    cty::c_uchar,
-    helpers::{bpf_d_path, bpf_ktime_get_ns},
+    cty::{c_uchar, c_void},
+    helpers::{bpf_d_path, bpf_ktime_get_ns, bpf_map_update_elem},
     macros::{fentry, fexit, map},
     maps::{Array, HashMap, PerCpuArray, RingBuf},
     programs::{FEntryContext, FExitContext},
 };
 use aya_log_ebpf::debug;
 use minicbor::Encoder;
-use nerscfslat_common::{FsWriteStats, NUM_PATH_PREFIX, PATHFRAGLEN};
+use nerscfslat_common::{EntryRec, FsWriteStats, NUM_PATH_PREFIX, PATHFRAGLEN};
 
 #[allow(nonstandard_style)]
 #[allow(unnecessary_transmutes)]
@@ -25,13 +27,13 @@ const BUFSIZE: usize = 1024;
 static COUNTER: Array<u64> = Array::with_max_entries(1, 0);
 
 #[map]
-static WRITESTATS: Array<FsWriteStats> = Array::with_max_entries(NUM_PATH_PREFIX, 0);
+static mut WRITESTATS: Array<FsWriteStats> = Array::with_max_entries(NUM_PATH_PREFIX, 0);
 
 #[map]
 static PATHBUF: PerCpuArray<[c_uchar; PATHFRAGLEN]> = PerCpuArray::with_max_entries(1, 0);
 
 #[map]
-static PTRLIST: HashMap<usize, u64> = HashMap::with_max_entries(1024, 0);
+static PTRLIST: HashMap<usize, EntryRec> = HashMap::with_max_entries(1024, 0);
 
 #[map]
 static LDMS_SHARED_STREAM: RingBuf = RingBuf::pinned(8192, 0);
@@ -49,7 +51,8 @@ fn hashmap_pathentry(
     pathlen: usize,
 ) -> Option<&FsWriteStats> {
     for idx in 0..NUM_PATH_PREFIX {
-        let Some(entry) = WRITESTATS.get(idx) else {
+        #[allow(static_mut_refs)]
+        let Some(entry) = (unsafe { WRITESTATS.get(idx) }) else {
             return None;
         };
         let pathstr = unsafe { core::str::from_utf8_unchecked(path) };
@@ -97,45 +100,93 @@ fn try_fslat_entry(ctx: FEntryContext) -> Result<u32, u32> {
         return Err(1);
     }
     // exclude trailing null
-    let Some(_) = hashmap_pathentry(
-        ctx,
-        unsafe { pathbuf_ptr.as_ref().unwrap() },
-        (ret - 1) as usize,
-    ) else {
-        return Ok(0);
-    };
-    let _ = PTRLIST.insert(&(filp as usize), &now, 0u64);
+//    let Some(entry) = hashmap_pathentry(
+//        ctx,
+//        unsafe { pathbuf_ptr.as_ref().unwrap() },
+//        (ret - 1) as usize,
+//    ) else {
+//        return Ok(0);
+//    };
+
+    {
+        let entryrec = EntryRec {
+            timestamp: now,
+            pathfrag: unsafe { *pathbuf_ptr },
+            fraglen: (ret - 1) as usize,
+        };
+        let _ = PTRLIST.insert(&(filp as usize), &entryrec, 0u64);
+    }
     Ok(0)
 }
 
 fn try_fslat_exit(ctx: FExitContext) -> Result<u32, u32> {
     let filp: *const vmlinux::file = ctx.arg(0);
-    let now = unsafe { bpf_ktime_get_ns() };
     let Some(countptr) = COUNTER.get_ptr_mut(0) else {
         return Err(1);
     };
     let eventf = EventFields {
         id: "fslat",
         version: "v1",
-        monotonic: now,
+        monotonic: unsafe { bpf_ktime_get_ns() },
         seq: unsafe { *countptr },
     };
 
     match unsafe { PTRLIST.get(filp as usize) } {
-        Some(nsecs) => {
-            let delta = now - nsecs;
-            if delta > 10 {
-                let Ok(_) = ringbuf_put(&eventf, "filp_close", delta, "ns") else {
-                    return Err(1);
-                };
-                let _ = PTRLIST.remove(&(filp as usize));
-            }
+        Some(entryrec) => {
+            let Ok(_) = update_stats(ctx, entryrec, "filp_close") else {
+                return Err(1);
+            };
+            let Ok(_) = ringbuf_put(&eventf, "filp_close", 0, "ns") else {
+                return Err(1);
+            };
+            let _ = PTRLIST.remove(&(filp as usize));
             unsafe {
                 *countptr += 1;
             }
         }
         None => {}
     };
+    Ok(0)
+}
+
+fn update_stats(ctx: FExitContext, entryrec: &EntryRec, op: &str) -> Result<u32, u32> {
+    let now = unsafe { bpf_ktime_get_ns() };
+    let delta = now - entryrec.timestamp;
+    for idx in 0..NUM_PATH_PREFIX {
+        #[allow(static_mut_refs)]
+        let Some(entry) = (unsafe { WRITESTATS.get(idx) }) else {
+            return Err(1);
+        };
+        let pathstr = unsafe { core::str::from_utf8_unchecked(&entryrec.pathfrag) };
+        let pathfragstr = unsafe { core::str::from_utf8_unchecked(&entry.pathfrag) };
+        let path = unsafe { pathstr.get_unchecked(..entryrec.fraglen.clamp(0, PATHFRAGLEN)) };
+        let pathfrag = unsafe { pathfragstr.get_unchecked(..entry.fraglen.clamp(0, PATHFRAGLEN)) };
+        debug!(ctx, "{}", path);
+        debug!(ctx, "-> {}", pathfrag);
+        if path.starts_with(pathfrag) && !pathfrag.is_empty() {
+            {
+                unsafe {
+                    let mut ws: FsWriteStats = *entry;
+                    if delta < ws.min {
+                        ws.min = delta;
+                    }
+                    if delta > ws.max {
+                        ws.max = delta;
+                    }
+                    ws.total += delta;
+                    ws.count += 1;
+                    #[allow(static_mut_refs)]
+                    bpf_map_update_elem(
+                        &raw mut WRITESTATS as *mut c_void,
+                        &raw const idx as *const c_void,
+                        &raw const ws as *const c_void,
+                        0u64,
+                    );
+                }
+            }
+        }
+    }
+
     Ok(0)
 }
 
